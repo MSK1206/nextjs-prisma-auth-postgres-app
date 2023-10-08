0655d2642216961f956825a1db5d416d5bb3b8d9
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-08 19:42:09 +0900

    Fix: import からtypeを削除

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import prisma from '@/lib/adapter/prisma';
 import GoogleProvider from 'next-auth/providers/google';
