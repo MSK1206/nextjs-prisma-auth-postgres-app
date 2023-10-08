6a647deba5c75b7cdfba8e277f225141ad32094b
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-08 19:26:58 +0900

    Add: plugin daisyui & extend animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,21 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        'rotate-center': 'rotate-center 10s linear  infinite both',
+      },
+      keyframes: {
+        'rotate-center': {
+          '0%': {
+            transform: 'rotate(0)',
+          },
+          to: {
+            transform: 'rotate(360deg)',
+          },
+        },
+      },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('daisyui')],
+};
+export default config;
