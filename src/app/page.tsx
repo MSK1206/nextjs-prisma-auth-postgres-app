7b7110b027f7b2ad537c55a2813aa0ebe5581f82
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-08 19:56:43 +0900

    Fix: callbackUrl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
                 <button
                   onClick={() =>
                     signIn('github', {
-                      callbackUrl: 'http://localhost:3000/loginok',
+                      callbackUrl: `${process.env.NEXTAUTH_URL}/login-ok`,
                     })
                   }
                   type="button"
