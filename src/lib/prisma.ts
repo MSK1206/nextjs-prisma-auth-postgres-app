83f53a1c7372b544d32102811535ec6bfcf42b08
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-09 15:20:19 +0900

    Add: comment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+// PrismaClientをインポートします。PrismaClientはPrismaを使用してデータベースとの通信を行うためのクライアントです。
+import { PrismaClient } from '@prisma/client';
 
+// グローバルスコープにPrismaClientのインスタンスを定義します
 declare global {
   var prisma: PrismaClient | undefined;
 }
-
+// グローバルスコープにPrismaClientのインスタンスがすでに存在するかどうかをチェックし、存在しない場合は新しいインスタンスを作成します。
 const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === "development") global.prisma = prisma;
+// 開発環境でのみ、グローバルスコープにPrismaClientのインスタンスを保存します。
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
 
 export default prisma;
