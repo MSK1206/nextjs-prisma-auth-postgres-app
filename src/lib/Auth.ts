f8d9617119832608bb293b4b9ac3844a69a4264c
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-12 11:25:10 +0900

    Add: PrismaAdapter , Providers config

---
+++ src/lib/Auth.ts
@@ -0,0 +1,21 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+
+const prisma = new PrismaClient();
+
+export const config = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
